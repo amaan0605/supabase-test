8cbaf1b1ed0ef3c088294e03199473c0077cec73
AMAAN ANSARI <99577880+amaan0605@users.noreply.github.com>
2024-08-22 23:30:53 +0530

    dark theme added

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade700,
+    primary: Colors.grey.shade800,
+    secondary: Colors.grey.shade900,
+    inversePrimary: Colors.grey.shade200,
+  ),
+);
